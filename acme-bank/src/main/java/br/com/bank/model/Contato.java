71c78a91371e2d7e5700ef31ec321dd59e6b32d3
udsonwillams <udson.willams@gmail.com>
2021-05-28 19:17:21 -0300

    ponto 1

--- acme-bank/src/main/java/br/com/bank/model/Contato.java
+++ acme-bank/src/main/java/br/com/bank/model/Contato.java
@@ -31,4 +31,30 @@ public class Contato {
 	private String nome;
 	
 	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
